53d9fc81ef66385187cc937a73277210aab7ac52
asathis2 <asathis2@illinois.edu>
2018-03-12 19:35:12 -0500

    final commit

--- mp2/part1/graph.py
+++ mp2/part1/graph.py
@@ -1,10 +1,11 @@
 class TreeNode():
-    def __init__(self, id,num, level, currl):
+    def __init__(self, id,num, level, currl, dist):
         self.children = {}
         self.id = id
         self.num=num
         self.level= level
         self.currl=currl
+        self.dist = dist
 
     def addchild(self,child, weight,letter):
         self.children[child]=(weight,letter)
--- mp2/part1/main2.py
+++ mp2/part1/main2.py
@@ -38,7 +38,7 @@ lets=['A','B','C','D','E']
 
 start=['', '', '', '', '']
 
-root= TreeNode(start,0,0,'')
+root= TreeNode(start,0,0,'',0)
 num=0
 graph[tuple(start)]= root
 creatq=queue.Queue()
@@ -56,7 +56,7 @@ for l in lets:
     if not ex:
         continue
     num=num+1
-    x = TreeNode(curr, num,1,l,)
+    x = TreeNode(curr, num,1,l,0)
     creatq.put(x)
     root.addchild(tuple(curr),0,l)
     graph[tuple(curr)]=x
@@ -68,6 +68,8 @@ while not creatq.empty():
         curr = copy.deepcopy(curr_beg.id)
         lvl=curr_beg.level
         ex = 0
+        curwt = curr_beg.dist
+
         for i in range(5):
             if curr[i] + l == facts[i][0:len(curr[i]) + 1]:
                 curr[i] = curr[i] + l
@@ -79,7 +81,7 @@ while not creatq.empty():
             curr_beg.addchild(tup, 0,l)
             continue
         num=num+1
-        x = TreeNode(curr, num,lvl+1,l)
+        x = TreeNode(curr, num,lvl+1,l,curwt+edges[(curr_beg.currl,l)])
         creatq.put(x)
         #print(curr,num)
         curr_beg.addchild(tup,edges[(curr_beg.currl,l)],l)
@@ -99,8 +101,7 @@ sq=''
 #     curr = frontier.get()
 #     sq=lq.get()
 #     if curr.id == facts:
-#         print(sq)
-#         break
+#         print(curr.dist,sq)
 #     for neighb, wt in curr.children.items():
 #         for n, m in graph.items():
 #             if n == neighb:
@@ -112,14 +113,18 @@ sq=''
 frontier = queue.PriorityQueue()
 frontier.put((0,graph[tuple(start)],'',0))
 seen=0
+expand=0
 while not frontier.empty():
     curr = frontier.get()
+    expand=expand+1
     seen=seen+1
     sq=curr[2]
     curwt=curr[3]
     if curr[1].id == facts:
         print("Lowest num of factories:")
         print(sq, len(sq), curwt)
+        print("Nodes expanded:")
+        print(expand)
         break
     for neighb, wt in curr[1].children.items():
         for n, m in graph.items():
@@ -131,21 +136,28 @@ while not frontier.empty():
 frontier = queue.PriorityQueue()
 frontier.put((0,graph[tuple(start)],'',0))
 seen=0
+expand=0
+#test=[]
 while not frontier.empty():
     curr = frontier.get()
+    expand=expand+1
+
     seen=seen+1
     sq=curr[2]
     curwt=curr[3]
     if curr[1].id == facts:
         print("Lowest num of miles")
         print(sq, len(sq), curwt)
+#        test.append(curwt)
+        print("Nodes expanded:")
+        print(expand)
         break
     for neighb, wt in curr[1].children.items():
         for n, m in graph.items():
             if n == neighb:
-                frontier.put((wt[0],m,sq+wt[1], curwt+wt[0]))
+                frontier.put((curwt+wt[0],m,sq+wt[1], curwt+wt[0]))
                 break
 
-
+#print(min(test))
 def heuristic1():
     return 1
